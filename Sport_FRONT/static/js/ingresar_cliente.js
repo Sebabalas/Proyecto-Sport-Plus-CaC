685664f34cff38d5c623b2d98e1d75585339e030
Sebabalas <sbals@frba.utn.edu.ar>
2024-07-01 16:40:38 -0300

    Agrego ingresar cliente js

---
+++ Sport_FRONT/static/js/ingresar_cliente.js
@@ -0,0 +1,48 @@
+function guardar() {
+    let id = document.getElementById("id").value
+    let nombre_ingresado = document.getElementById("nombre").value.trim(); 
+    let mail_ingresado = document.getElementById("mail").value.trim();
+    let telefono_ingresado = document.getElementById("tel").value.trim()
+    let genero_ingresado = obtenerGeneroSeleccionado();
+    let servicios_seleccionados = document.querySelectorAll('input[name="servicios"]:checked');
+    let plan_seleccionado = document.getElementById("plan").value;
+    let observaciones_consulta = document.getElementById("consulta-textarea").value;
+    let tieneAptoFisico = document.getElementById("aptofisico").value;
+
+    let datos = {
+        nombre: nombre_ingresado,
+        mail: mail_ingresado,
+        tel: telefono_ingresado,
+        genero: genero_ingresado,
+        servicios: servicios_seleccionados,
+        plan: plan_seleccionado,
+        consulta: observaciones_consulta,
+        aptofisico: tieneAptoFisico
+    };
+    console.log(datos);
+    
+    enviarDatosAlBackEnd(datos);
+
+}
+
+function enviarDatosAlBackEnd(datos){
+        let url = "http://localhost:5000/cliente/"
+        var options = {
+            body: JSON.stringify(datos),
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+        }
+        fetch(url, options)
+            .then(function () {
+                console.log("creado")
+                alert("Grabado")
+                // Devuelve el href (URL) de la página actual
+                window.location.href = "../tabla_clientes.html";  
+                
+            })
+            .catch(err => {
+                //this.errored = true
+                alert("Error al grabar" )
+                console.error(err);
+            })
+    }
