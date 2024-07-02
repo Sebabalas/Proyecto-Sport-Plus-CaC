9d3981e6291f6d4a1e4c1ec5789993abea79750d
Alejandro <alejandro.pisera@bue.edu.ar>
2024-07-02 18:40:10 -0300

    login con localstorage

--- Sport_FRONT/static/js/login_clientes.js
+++ Sport_FRONT/static/js/login_clientes.js
@@ -2,6 +2,25 @@ const form = document.querySelector('form');
 const sectionLogin = document.getElementById('container_login');
 const sectionConsultas = document.getElementById('container_consultasclientes');
 
+// Función para guardar los datos de acceso en localStorage
+function guardarDatosAcceso(username) {
+    localStorage.setItem('username', username);
+}
+
+// Función para comprobar si hay datos de acceso guardados en localStorage
+function comprobarDatosAccesoGuardados() {
+    const username = localStorage.getItem('username');
+    if (username) {
+        // Mostrar la sección de consultas de clientes si hay un usuario guardado
+        sectionConsultas.style.display = 'flex';
+        // Ocultar la sección de login
+        sectionLogin.style.display = 'none';
+    }
+}
+
+// Comprobar si ya hay datos de acceso guardados al cargar la página
+comprobarDatosAccesoGuardados();
+
 form.addEventListener('submit', function(event) {
     event.preventDefault(); // Evitar el envío del formulario por defecto
 
@@ -9,6 +28,9 @@ form.addEventListener('submit', function(event) {
     const password = document.getElementById('password').value;
 
     if (username === 'admin' && password === '1234') {
+        // Guardar el nombre de usuario en localStorage
+        guardarDatosAcceso(username);
+
         // Mostrar la sección de consultas de clientes si el inicio de sesión es exitoso
         sectionConsultas.style.display = 'flex';
         // Ocultar la sección de login
