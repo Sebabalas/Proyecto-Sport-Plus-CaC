12f18a83e7d58fc6c2c136a33448fb8fd31dc45e
Alejandro <alejandro.pisera@bue.edu.ar>
2024-07-03 17:24:32 -0300

    metodo PUT funciona ok

--- Sport_FRONT/static/js/editar_cliente.js
+++ Sport_FRONT/static/js/editar_cliente.js
@@ -1,56 +1,51 @@
 function modificar() {
-    let id = document.getElementById("id").value
+    let id = document.getElementById("id").value;
     let nombre_ingresado = document.getElementById("nombre").value.trim(); 
     let mail_ingresado = document.getElementById("mail").value.trim();
-    let telefono_ingresado = document.getElementById("tel").value.trim()
+    let telefono_ingresado = document.getElementById("telefono").value.trim();
     let genero_ingresado = obtenerGeneroSeleccionado();
-    let servicios_seleccionados = document.querySelectorAll('input[name="servicios"]:checked');
+    let servicios_seleccionados = Array.from(document.querySelectorAll('input[name="servicios"]:checked')).map(checkbox => checkbox.value);
     let plan_seleccionado = document.getElementById("plan").value;
     let observaciones_consulta = document.getElementById("consulta-textarea").value;
     let tieneAptoFisico = document.getElementById("aptofisico").value;
 
     let datos = {
+        id: id,
         nombre: nombre_ingresado,
         mail: mail_ingresado,
         tel: telefono_ingresado,
         genero: genero_ingresado,
-        servicios: servicios_seleccionados,
+        servicios: servicios_seleccionados.join(","),
         plan: plan_seleccionado,
         consulta: observaciones_consulta,
         aptofisico: tieneAptoFisico
-    }
-    console.log(datos);
-    enviarDatosAlBackEnd(datos);
+    };
 
+    console.log(datos);
+    enviarDatosAlBackEnd(datos, id);
 }
 
-function enviarDatosAlBackEnd(datos){
-        let url = "http://127.0.0.1:5000/clientes/"+id
-        var options = {
-            body: JSON.stringify(datos),
-            method: 'PUT',
-            
-            headers: { 'Content-Type': 'application/json' },
-            // el navegador seguirá automáticamente las redirecciones y
-            // devolverá el recurso final al que se ha redirigido.
-            redirect: 'follow'
-        }
-        fetch(url, options)
-            .then(function () {
-                console.log("modificado")
-                alert("Registro modificado")
-    
-                //Puedes utilizar window.location.href para obtener la URL actual, redirigir a otras páginas
-               window.location.href = "../../templates/admin/tabla_clientes.html";
-              
-            })
-            .catch(err => {
-                this.error = true
-                console.error(err);
-                alert("Error al Modificar")
-            })      
-    }
+function enviarDatosAlBackEnd(datos, id){
+    let url = "http://127.0.0.1:5000/clientes/" + id;
+    var options = {
+        body: JSON.stringify(datos),
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        redirect: 'follow'
+    };
+    fetch(url, options)
+        .then(function () {
+            console.log("modificado");
+            alert("Registro modificado");
 
+            // Redirigir a la página de clientes
+            window.location.href = "../../templates/admin/tabla_clientes.html";
+        })
+        .catch(err => {
+            console.error(err);
+            alert("Error al Modificar");
+        });
+}
 
 function obtenerGeneroSeleccionado() {
         // Obtener todos los elementos de radio con name="genero"
